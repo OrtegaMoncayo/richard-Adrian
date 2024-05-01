fa46f06a60251eca8af900b9fc074682b6060fe0
OrtegaMoncayo <richard.ortega778@ist17dejulio.edu.ec>
2024-05-01 15:39:55 -0500

    proyecto inicial

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -4,13 +4,16 @@
  */
 package vista;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alejandro
  */
 public class Main {
+
     //MÉTODO LLAMADO INTERPRETE DE JAVA
-    public static void main(String[]args){
+    public static void main(String[] args) {
         //1.- DECLARACIÓN DE VARIABLES A TRAVÉS DE DATOS PRIMITIVOS
         //NÚMEROS ENTEROS
 //        int numero = 0;
@@ -23,46 +26,56 @@ public class Main {
 //        
 //        //MODIFICAR VALORES
 //        numero = 15;
-        
+
         //2.- IMPRIMIR EN CONSOLA
         //System.out.println("El valor del entero es: " + numero);
         //System.out.println("El valor del decimal es: " + decimales);
         //System.out.println("El valor del caracter es: " + caracter);
         //System.out.println("El valor del booleaneo es: " + booleaneo);
-
         //3. DECLARACIÓN DE OBJETOS
         //TIPO TEXTO
         //String malasNoticias = "Hoy es viernes y mañana hay clases";
         //System.out.println("El contenido de la cadena es: " + malasNoticias);
-        
-        
         //EJERCICIO POBLACIONAL, VAMOS A SIMULAR LA POBLACIÓN DE
         //CONEJOS. POR LO QUE NECESITO CONOCER LOS SIGUIENTES DATOS:
         //1.- EL NÚMERO DE PAREJAS DE CONEJOS
         //2.- EL NÚMERO DE CRÍAS POR PAREJA
         //3.- EL PORCENTAJE DE TASA DE MORTALIDAD ANUAL
         //4.- SIMULAR DURANTE UN PERIODO DE TIEMPO
-        
-        int numeroParejas = 2;
-        int numeroCrias = 0;
-        double tasaMortalidad = 20;
-        int años = 3;
-        int poblacion;
-        int poblacionFinal;
-        
-        double tasaMortalidadDecimal = (100 - tasaMortalidad) / 100;
-        
-        for (int i = 0; i <= años; i++) {
-            poblacion = (numeroParejas * 2);
-            if (i > 0) {
-                poblacion += (numeroParejas * numeroCrias);
+        Scanner s = new Scanner(System.in);
+        //imprime en consola un mensaje
+        System.out.println("Ingrese el numero de parejas");
+        //capturar el mensaje
+        int parejas = s.nextInt();
+        // imprimo mensaje
+        System.out.println("ingrese el numero de crias");
+        //captura la entrada realizada pro el usuario mediante teclado
+        int nCrias = s.nextInt();
+
+        double tMortalidad = 20;
+        int periodo = 5;
+        int pAnual = 0;
+        int pTotal = 0, pMorir = 0, crias = 0;
+
+        for (int i = 0; i <= periodo; i++) {
+            if (i == 0) {
+                pAnual = parejas * 2;
+                pMorir = (int) (pAnual * (tMortalidad) / 100);
+                pTotal = pAnual - pMorir;
+            } else {
+                crias = parejas * nCrias;
+                pAnual += crias;
+                pMorir = (int) (pAnual * (tMortalidad) / 100);
+                pTotal = pAnual - pMorir;
+                parejas = pTotal / 2;
+
             }
-            
-            poblacionFinal = (int) Math.round(poblacion * tasaMortalidadDecimal);
-            numeroParejas = (int) Math.round(poblacionFinal / 2);
-            
-            System.out.println("La población en el año " + i + " es: " + poblacion);
-            System.out.println("Debido a la tasa de mortalidad, la población bajó a: " + poblacionFinal);
-        }   
+            System.out.println("Año:" + i + "\n"
+                    + "Población Actual es:" + pAnual);
+            System.out.println("Población Morir es:" + pMorir);
+            System.out.println("Población Total es:" + pTotal);
+            System.out.println("El Número de parejas es:" + parejas);
+            System.out.println("Número de Crias anuales:" + crias);
+        }
     }
 }
